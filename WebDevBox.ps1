333ae82f3d8eae0d87287c8be3cc43999c6f95c3
Pontus Frendin <pontus@frend.in>
2016-12-22 11:29:27 +0100

    Uppdaterar NuGet.Config

--- WebDevBox.ps1
+++ WebDevBox.ps1
@@ -28,3 +28,5 @@ cmd /c sc config aspnet_state start= auto
 cmd /c net start aspnet_state
 $npm = Join-Path -Path $env:ProgramFiles -ChildPath nodejs\npm
 & $npm install --global gulp-cli
+$nugetconfig = Join-Path $env:AppData -ChildPath NuGet\NuGet.Config
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/pnotus/WebDevBox/master/NuGet.Config", $nugetconfig)
