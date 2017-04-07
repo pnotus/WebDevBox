a9552500a742258fae9fea61e68a018d8ea42d46
Pontus Frendin <pontus@frend.in>
2017-04-07 16:46:32 +0200

    Win10-anpassat scriptet

--- WebDevBox.ps1
+++ WebDevBox.ps1
@@ -1,6 +1,5 @@
 Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
 Set-TaskbarOptions -Size Small -Lock -Combine Never
-cinst visualstudio2015enterprise -packageParameters "--AdminFile https://raw.githubusercontent.com/pnotus/WebDevBox/master/AdminDeployment.xml"
 cinst git
 cinst nodejs
 cinst sysinternals
@@ -11,22 +10,17 @@ cinst firefox
 cinst googlechrome
 cinst paint.net
 cinst 7zip
+cinst kdiff3
 cinst visualstudiocode
-cinst tfs2015powertools
-cinst IIS-WebServerRole -source windowsfeatures
-cinst IIS-HttpRedirect -source windowsfeatures
-cinst IIS-ClientCertificateMappingAuthentication -source windowsfeatures
-cinst IIS-IISCertificateMappingAuthentication -source windowsfeatures
-cinst IIS-WindowsAuthentication -source windowsfeatures
-cinst IIS-ISAPIFilter -source windowsfeatures
-cinst IIS-ISAPIExtensions -source windowsfeatures
-cinst IIS-NetFxExtensibility -source windowsfeatures
-cinst IIS-ASPNET -source windowsfeatures
-$aspnet_regiis = Join-Path -Path $env:WinDir -ChildPath Microsoft.NET\Framework\v4.0.30319\aspnet_regiis.exe
-& $aspnet_regiis -I
-cmd /c sc config aspnet_state start= auto
-cmd /c net start aspnet_state
-$npm = Join-Path -Path $env:ProgramFiles -ChildPath nodejs\npm
-& $npm install --global gulp-cli
+cinst visualstudio2017enterprise
+cinst visualstudio2017-workload-azure
+cinst visualstudio2017-workload-data
+cinst visualstudio2017-workload-manageddesktop
+cinst visualstudio2017-workload-netcoretools
+cinst visualstudio2017-workload-netweb
+cinst visualstudio2017-workload-node
+cinst visualstudio2017-workload-office
+cinst visualstudio2017-workload-universal
 $nugetconfig = Join-Path $env:AppData -ChildPath NuGet\NuGet.Config
 (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/pnotus/WebDevBox/master/NuGet.Config", $nugetconfig)
+cinst resharper
