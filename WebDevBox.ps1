3af73bfefdf02abcea44af40101f00ee75aa2092
Pontus Frendin <pontus@frend.in>
2017-05-23 22:17:47 +0200

    Skapar script för hemmadator

--- WebDevBox.ps1
+++ WebDevBox.ps1
@@ -12,18 +12,9 @@ cinst paint.net
 cinst 7zip
 cinst kdiff3
 cinst visualstudiocode
-cinst visualstudio2017enterprise
+cinst visualstudio2017community
 cinst visualstudio2017-workload-azure
-cinst visualstudio2017-workload-data
-cinst visualstudio2017-workload-manageddesktop
 cinst visualstudio2017-workload-netcoretools
 cinst visualstudio2017-workload-netweb
 cinst visualstudio2017-workload-node
-cinst visualstudio2017-workload-office
-cinst visualstudio2017-workload-universal
-$nugetconfig = Join-Path $env:AppData -ChildPath NuGet\NuGet.Config
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/pnotus/WebDevBox/master/NuGet.Config", $nugetconfig)
-cmd /c sc config aspnet_state start=auto
-cmd /c net start aspnet_state
-$npm = Join-Path -Path $env:ProgramFiles -ChildPath nodejs\npm
-& $npm install --global gulp-cli
+cinst chocolatey
